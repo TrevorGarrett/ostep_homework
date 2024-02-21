a49b35e850a80c16702508a63030d1c902bed36a
Trevor Garrett <tgarrett6204@gmail.com>
2024-02-21 09:20:11 -0500

    Add fork_opened_file function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -23,7 +24,35 @@ int fork_with_flags(){
     return 0;
 }
 
+// Both the parent and child process write to the same file after being forked,
+// even though the file descriptors pointing to the new file would be two distinct memory objects,
+// they would point to the same file.
+int fork_opened_file(){
+    // Close standard output file descriptor
+    close(STDOUT_FILENO);
+    // Open a file descriptor that is write only (O_WRONLY), created if it doesn't exist (O_CREAT), 
+    // and truncated to 0 if a file was successfully opened (O_TRUNC). 
+    // We are also setting the mode argument to allow the user to read, write and execute the file (S_IRWXU)
+    open("fork_opened_file.output", O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+    int fork_rc = fork();
+    if (fork_rc < 0) {
+        fprintf(stderr, "Fork failed\n");
+        return 1;
+    } else if (fork_rc == 0) {
+        printf("Write to file from child\n");
+    } else {
+        printf("Write to file from parent\n");
+    }
+    return 0;
+}
+
 int main(int argc, char **argv){
-    fork_with_flags();
+    // Homework for process_api
+
+    // #1
+    // fork_with_flags();
+
+    // #2
+    fork_opened_file();
     return 0;
 }
